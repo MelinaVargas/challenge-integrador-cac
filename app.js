695cd6eca1780453605678d015bb775c52ca2613
Melina Vargas <melina97marialuz@gmail.com>
2023-11-09 08:08:16 -0300

    [FEAT] routes and controllers for the application

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const app = express();
+const mainRoutes = require("./src/routes/mainRoutes");
 
 app.use(express.static("public"));
+
+app.use("/", mainRoutes);
+app.use("/", shopRoutes);
 
 app.listen(4000, () =>
   console.log("Servidor corriendo en http://localhost:4000")
